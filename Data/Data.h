8b4be998c73bc3ce0696f9275156d547c55c08a7
atsushi130 <atsushi_main@outlook.jp>
2019-05-05 23:01:17 +0900

    add: Data target

---
+++ Data/Data.h
@@ -0,0 +1,19 @@
+//
+//  Data.h
+//  Data
+//
+//  Created by Atsushi Miyake on 2019/05/05.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Data.
+FOUNDATION_EXPORT double DataVersionNumber;
+
+//! Project version string for Data.
+FOUNDATION_EXPORT const unsigned char DataVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Data/PublicHeader.h>
+
+
