f262568a94cb7071a5e071b54511c8cdd8c1643f
atsushi130 <atsushi_main@outlook.jp>
2019-05-05 23:00:41 +0900

    add: Shared target

---
+++ Shared/Shared.h
@@ -0,0 +1,19 @@
+//
+//  Shared.h
+//  Shared
+//
+//  Created by Atsushi Miyake on 2019/05/05.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Shared.
+FOUNDATION_EXPORT double SharedVersionNumber;
+
+//! Project version string for Shared.
+FOUNDATION_EXPORT const unsigned char SharedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Shared/PublicHeader.h>
+
+
