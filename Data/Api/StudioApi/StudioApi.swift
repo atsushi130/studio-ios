afa9a316feddbbd286361c1490153685ed208324
atsushi130 <atsushi_main@outlook.jp>
2019-05-05 23:02:02 +0900

    define: StudioApi

---
+++ Data/Api/StudioApi/StudioApi.swift
@@ -0,0 +1,17 @@
+//
+//  StudioApi.swift
+//  Data
+//
+//  Created by Atsushi Miyake on 2019/05/05.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
+
+public struct StudioApi {}
+
+public extension StudioApi {
+    struct Error {
+        public let message: String
+    }
+}
