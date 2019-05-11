65bd50f05c8d0d504320ac081cd7530616ae3a43
atsushi130 <atsushi_main@outlook.jp>
2019-05-11 21:35:07 +0900

    add: new files

---
+++ Extension/RoutableViewController+Configurator.swift
@@ -0,0 +1,9 @@
+//
+//  RoutableViewController+Configurator.swift
+//  Extension
+//
+//  Created by Atsushi Miyake on 2019/05/11.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
---
+++ Extension/ViewModelInjectable.swift
@@ -0,0 +1,9 @@
+//
+//  ViewModelInjectable.swift
+//  Extension
+//
+//  Created by Atsushi Miyake on 2019/05/11.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
