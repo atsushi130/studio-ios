268e55720fd01139e63184e97d5443770fb9c06b
atsushi130 <atsushi_main@outlook.jp>
2019-05-12 00:37:34 +0900

    implement: coordinate launch view controller

--- studio/AppDelegate.swift
+++ studio/AppDelegate.swift
@@ -12,10 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    private lazy var coordinator = ApplicationCoordinator(window: self.window)
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.coordinator.transition(to: .studioTimeTable)
         return true
     }
 
