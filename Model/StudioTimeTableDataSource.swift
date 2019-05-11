b2fe125bf18f50ef917a641cc6f72f3893d9c084
atsushi130 <atsushi_main@outlook.jp>
2019-05-11 19:29:01 +0900

    define: StudioTimeTableDataSource

--- Model/StudioTimeTableDataSource.swift
+++ Model/StudioTimeTableDataSource.swift
@@ -1,14 +1,33 @@
 //
-//  StudioTimeTableSectionModel.swift
+//  StudioTimeTableDataSource.swift
 //  Model
 //
 //  Created by Atsushi Miyake on 2019/05/11.
 //  Copyright © 2019 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
 import RxDataSources
 
 public enum StudioTimeTableSectionModel {
+    case section(items: [StudioTimeTableSectionItem])
+}
+
+public enum StudioTimeTableSectionItem {
+    case item(item: StudioSchedule)
+}
+
+extension StudioTimeTableSectionModel: SectionModelType {
     
+    public typealias Item = StudioTimeTableSectionItem
+    
+    public var items: [Item] {
+        switch self {
+        case let .section(items):
+            return items
+        }
+    }
+    
+    public init(original: StudioTimeTableSectionModel, items: [Item]) {
+        self = original
+    }
 }
