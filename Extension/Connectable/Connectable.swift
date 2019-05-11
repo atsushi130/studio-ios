a9b167974cbc8ca856361ddf0c1074fc5edd9236
atsushi130 <atsushi_main@outlook.jp>
2019-05-11 21:49:49 +0900

    implement: Connectable

---
+++ Extension/Connectable/Connectable.swift
@@ -0,0 +1,9 @@
+//
+//  Connectable.swift
+//  Extension
+//
+//  Created by Atsushi Miyake on 2019/05/11.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+public typealias Connectable = Inputable & Outputable
