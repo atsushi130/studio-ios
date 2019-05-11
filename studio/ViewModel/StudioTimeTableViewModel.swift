0e6817b0251f1ad8df6a7f2a257dedf871ed78e2
atsushi130 <atsushi_main@outlook.jp>
2019-05-11 23:27:16 +0900

    implement: StudioTimeTableViewModel

--- studio/ViewModel/StudioTimeTableViewModel.swift
+++ studio/ViewModel/StudioTimeTableViewModel.swift
@@ -7,8 +7,49 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+import Model
+import Extension
+import Shared
 
-final class StudioTimeTableViewModel {
-    init() {
+final class StudioTimeTableViewModel: Connectable {
+    
+    fileprivate let selectedStudioSchedule = PublishSubject<StudioSchedule>()
+    fileprivate let sectionModel: Observable<[StudioTimeTableSectionModel]>
+    
+    let coordinator: StudioTimeTableCoordinator
+    
+    init(coordinator: StudioTimeTableCoordinator) {
+        
+        self.coordinator = coordinator
+        
+        self.sectionModel = StudioReservationManager.shared.reservations
+            .map { reservations -> [String: [StudioReservation]] in
+                Dictionary(grouping: reservations) { $0.start }
+            }
+            .map { groupedReservations in
+                StudioTimeTable.allCases
+                    .map { timeTable -> StudioSchedule in
+                        StudioScheduleFactory.make(timeTable: timeTable, groupedReservations: groupedReservations)
+                    }
+                    .map { studioSchedule -> StudioTimeTableSectionItem in
+                        .item(item: studioSchedule)
+                    }
+            }
+            .map { items -> [StudioTimeTableSectionModel] in
+                [.section(items: items)]
+            }
+        
+        // sync reservations
+        StudioReservationManager.shared.refetchTodayReservations()
     }
+}
+
+extension InputSpace where Definer: StudioTimeTableViewModel {
+    var selectedStudioSchedule: AnyObserver<StudioSchedule> { return self.definer.selectedStudioSchedule.asObserver() }
+}
+
+extension OutputSpace where Definer: StudioTimeTableViewModel {
+    var sectionModel: Observable<[StudioTimeTableSectionModel]> { return self.definer.sectionModel }
 }
