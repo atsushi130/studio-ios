2d314b5a889edf9ecc5359d5a45266151cd8ff42
atsushi130 <atsushi_main@outlook.jp>
2019-05-11 21:35:28 +0900

    implement: RoutableViewController and configurator

--- Extension/RoutableViewController+Configurator.swift
+++ Extension/RoutableViewController+Configurator.swift
@@ -6,4 +6,39 @@
 //  Copyright © 2019 Atsushi Miyake. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public protocol RoutableViewController where Self: UIViewController {
+    associatedtype ViewControllerConfigurator: Configurator
+    associatedtype Dependency
+    static func instantiate(with dependency: Dependency) -> Self
+}
+
+public extension RoutableViewController where ViewControllerConfigurator.VC == Self {
+    static func instantiate(with dependency: Dependency) -> Self {
+        let className  = String(describing: self)
+        let storyboard = UIStoryboard(name: className, bundle: Bundle(for: self))
+        let vc = storyboard.instantiateViewController(withIdentifier: className) as! Self
+        ViewControllerConfigurator.configure(with: vc, dependency: dependency)
+        return vc
+    }
+}
+
+public extension RoutableViewController where ViewControllerConfigurator.VC == Self, Dependency == Void {
+    static func instantiate() -> Self {
+        let className  = String(describing: self)
+        let storyboard = UIStoryboard(name: className, bundle: Bundle(for: self))
+        let vc = storyboard.instantiateViewController(withIdentifier: className) as! Self
+        ViewControllerConfigurator.configure(with: vc, dependency: ())
+        return vc
+    }
+}
+
+public protocol Configurator {
+    associatedtype VC: RoutableViewController
+    static func configure(with vc: VC, dependency: VC.Dependency)
+}
+
+public extension Configurator {
+    static func configure(with vc: VC, dependency: VC.Dependency) {}
+}
