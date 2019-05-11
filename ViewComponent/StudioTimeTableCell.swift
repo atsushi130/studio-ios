7f9a278e91ecda219e6561439a557f5d117f8665
atsushi130 <atsushi_main@outlook.jp>
2019-05-12 01:26:43 +0900

    add: StudioTimeTableCeii

---
+++ ViewComponent/StudioTimeTableCell.swift
@@ -0,0 +1,9 @@
+//
+//  StudioTimeTableCell.swift
+//  ViewComponent
+//
+//  Created by Atsushi Miyake on 2019/05/12.
+//  Copyright © 2019 Atsushi Miyake. All rights reserved.
+//
+
+import Foundation
