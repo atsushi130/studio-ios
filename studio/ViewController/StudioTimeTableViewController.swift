f029077ff86b0835d6acc776f700cb013c29dff1
atsushi130 <atsushi_main@outlook.jp>
2019-05-12 16:08:59 +0900

    implement: reserve studio when tapped schedule

--- studio/ViewController/StudioTimeTableViewController.swift
+++ studio/ViewController/StudioTimeTableViewController.swift
@@ -14,6 +14,7 @@ import SwiftExtensions
 import Model
 import Extension
 import ViewComponent
+import Shared
 
 extension StudioTimeTableViewController: RoutableViewController {
     typealias ViewControllerConfigurator = StudioTimeTableConfigurator
@@ -24,9 +25,17 @@ final class StudioTimeTableViewController: UIViewController, ViewModelInjectable
  
     @IBOutlet weak var collectionView: UICollectionView! {
         didSet {
+            self.collectionView.collectionViewLayout = self.layout
             self.collectionView.register(cellType: StudioTimeTableCell.self)
         }
     }
+    
+    private lazy var layout: UICollectionViewFlowLayout = {
+        let layout = UICollectionViewFlowLayout()
+        // self sizing by autolayout
+        layout.estimatedItemSize = CGSize(width: 375, height: 100)
+        return layout
+    }()
     
     private typealias DataSource = RxCollectionViewSectionedReloadDataSource<StudioTimeTableSectionModel>
     private lazy var dataSource = DataSource(configureCell: { [weak self] (dataSource, tableView, indexPath, _) in
@@ -34,9 +43,8 @@ final class StudioTimeTableViewController: UIViewController, ViewModelInjectable
         let item = dataSource[indexPath]
         switch item {
         case let .item(studioSchedule):
-            let availables = studioSchedule.availables
             let cell = self.collectionView.dequeueReusableCell(with: StudioTimeTableCell.self, for: indexPath)
-            cell.startTimeLabel.text = "\(studioSchedule.startDate)"
+            cell.studioSchedule = studioSchedule
             return cell
         }
     })
@@ -61,7 +69,30 @@ final class StudioTimeTableViewController: UIViewController, ViewModelInjectable
         
         self.collectionView.rx.modelSelected(StudioTimeTableSectionItem.self)
             .map { $0.item }
+            .flatMap { item -> Observable<StudioSchedule> in
+                let result = PublishSubject<StudioSchedule>()
+                let hour =  NSString(format: "%02d", item.startDate.hour)
+                let minute = NSString(format: "%02d", item.startDate.minute)
+                let alert = UIAlertController(title: "スタジオ予約", message: "\(hour):\(minute)からスタジオを予約しますか？", preferredStyle: .alert)
+                let reserve = UIAlertAction(title: "予約する", style: .default) { _ in
+                    result.onNext(item)
+                }
+                let cancel = UIAlertAction(title: "しない", style: .cancel)
+                alert.addAction(reserve)
+                alert.addAction(cancel)
+                self.present(alert, animated: true)
+                return result
+            }
             .bind(to: self.viewModel.in.selectedStudioSchedule)
+            .disposed(by: self.disposeBag)
+        
+        self.viewModel.out.reservedStudio
+            .do(onNext: { _ in
+                let alert = UIAlertController.okAlertController(title: "予約が完了しました！")
+                self.present(alert, animated: true)
+            })
+            .flatMap { StudioReservationManager.shared.refetchTodayReservations() }
+            .subscribe()
             .disposed(by: self.disposeBag)
     }
 }
