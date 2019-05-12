de8077707267607a944cb1734bb5b0ba3bd51153
atsushi130 <atsushi_main@outlook.jp>
2019-05-12 16:06:50 +0900

    temp implement: StudioTimeTableCell

--- ViewComponent/StudioTimeTableCell.swift
+++ ViewComponent/StudioTimeTableCell.swift
@@ -7,8 +7,28 @@
 //
 
 import UIKit
+import Model
+import Extension
+import SwiftExtensions
 
+@IBDesignable
 public final class StudioTimeTableCell: UICollectionViewCell {
     
-    @IBOutlet public weak var startTimeLabel: UILabel!
+    @IBOutlet weak var startTimeLabel: UILabel!
+    @IBOutlet weak var studioA: CircleLabelView!
+    @IBOutlet weak var studioB: CircleLabelView!
+    @IBOutlet weak var studioC: CircleLabelView!
+    
+    public var studioSchedule: StudioSchedule! {
+        didSet {
+            let startDate = self.studioSchedule.startDate
+            let hour = NSString(format: "%02d", startDate.hour)
+            let minute = NSString(format: "%02d", startDate.minute)
+            self.startTimeLabel.text = "\(hour):\(minute)"
+            let availables = self.studioSchedule.availables
+            self.studioA.alpha = availables.studioA ? 1 : 0.25
+            self.studioB.alpha = availables.studioB ? 1 : 0.25
+            self.studioC.alpha = availables.studioC ? 1 : 0.25
+        }
+    }
 }
